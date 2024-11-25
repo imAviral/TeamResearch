1c7ce5bfe34bf68ef4c5210f0d7ccbb11bdd35d0
haseebakhter14 <haseebakhter14@gmail.com>
2024-11-25 08:22:51 +0000

    soybean and soybean oil data visualization diagram

--- README.md
+++ README.md
@@ -12,7 +12,8 @@ Comparison of Corn, Oats, Cereals, and Grains Futures Data in Detail
 [![Wheat Crop](https://i.postimg.cc/gkxMygxr/temp-Imagefh-Ps-QA.avif)](https://postimg.cc/4mTbsbbT)
 [![Oat Crop](https://i.postimg.cc/1tyqtGTz/Screenshot-2024-11-24-at-21-53-36.png)](https://postimg.cc/NyPMCHQh)
 [![Rough Rice Crop](https://i.postimg.cc/Bv46cqCY/Screenshot-2024-11-24-at-22-17-57.png)](https://postimg.cc/K1pxmhwB)
-![App Screenshot]()
+[![Soybean Oil Crop](https://i.postimg.cc/85dxdStH/Screenshot-2024-11-24-at-22-42-56.png)](https://postimg.cc/vgBPMpRg)
+[![Soybean Crop](https://i.postimg.cc/rFw747W1/Screenshot-2024-11-24-at-22-46-38.png)](https://postimg.cc/V527QGKv)
 [![All Crop](https://i.postimg.cc/Y0vqqR6Y/Screenshot-2024-11-25-at-00-14-09.png)](https://postimg.cc/K3yy0ns8)
 
 ## Feedback
---
+++ individual_code/Soybean-oil.R
@@ -0,0 +1,40 @@
+# This R environment comes with many helpful analytics packages installed
+library(readr)
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+
+# reading file data
+soybean_oil_data <- read_csv("../individual_data/Soybean_Oil_data.csv",show_col_types = FALSE)
+# Check the structure of the data
+str(soybean_oil_data)
+
+# Ensure the date column is in Date format
+soybean_oil_data$date <- as.Date(soybean_oil_data$date, format = "%Y-%m-%d")
+
+# Aggregate the data to get the average close for each date (if there are duplicates)
+soybean_oil_data_agg <- soybean_oil_data %>%
+  group_by(date) %>%
+  summarise(close = mean(close, na.rm = TRUE))
+
+# Plot close vs date as a line graph
+ggplot(soybean_oil_data_agg, aes(x = date, y = close)) +
+  geom_line(color = "#F1C232") +  # Line graph
+  labs(title = "Close vs Date", x = "Date", y = "Close") +
+  theme_minimal()
+
+# Plot histogram with density curve line
+ggplot(soybean_oil_data_agg, aes(x = close)) +
+  geom_histogram(aes(y = ..density..), binwidth = 1, fill = "#F1C232", color = "#F1C232", alpha = 0.5) +  # Histogram
+  geom_density(color = "#FF0000", size = 1) +  # Density curve line
+  labs(title = "Histogram with Density Curve Line", x = "Close", y = "Density") +
+  theme_minimal()
+
+# Scatter Plot of Close by date
+ggplot(soybean_oil_data_agg, aes(x = date, y = close)) +
+  geom_point(color = "#FFD700", size = 2) +  # Scatter plot with blue points
+  geom_smooth(method = "lm", color = "red", linewidth = 1) +  # Use 'linewidth' instead of 'size' for the regression line
+  labs(title = "Scatter Plot of Close by date with Regression Line",
+       x = "date",
+       y = "Close") +
+  theme_minimal()
